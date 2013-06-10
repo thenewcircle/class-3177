121911dc475478b309c0175427b94ba09434e059
Marko Gargenta <marko@marakana.com>
2013-06-10 15:18:04 -0700

    Basic UI and UI handling

--- Yamba/src/com/twitter/yamba/StatusActivity.java
+++ Yamba/src/com/twitter/yamba/StatusActivity.java
@@ -1,23 +1,63 @@
 package com.twitter.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class StatusActivity extends Activity {
+	private Button mButtonTweet;
+	private EditText mTextStatus;
+	private TextView mTextCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-    }
+        
+        mButtonTweet = (Button) findViewById(R.id.status_button_tweet);
+        mTextStatus = (EditText) findViewById(R.id.status_text);
+        mTextCount = (TextView) findViewById(R.id.status_text_count);
+        mTextCount.setText( Integer.toString(140) );
+        
+        mButtonTweet.setOnClickListener( new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				String status = mTextStatus.getText().toString();
+				
+				Log.d("StatusActivity", "I got clicked with status: "+status);
+			}
+        	
+        });
+        
+        mTextStatus.addTextChangedListener( new TextWatcher() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
+			@Override
+			public void afterTextChanged(Editable s) {
+				int count = 140 - s.length();
+				mTextCount.setText( Integer.toString(count) );
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+			}
+        	
+        });
     }
     
+
 }
