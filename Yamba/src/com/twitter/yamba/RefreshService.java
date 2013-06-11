63bb241fb33c93781674dee753c72a8cbabd65a0
Marko Gargenta <marko@marakana.com>
2013-06-11 16:58:00 -0700

    Implemented RefreshService

--- Yamba/src/com/twitter/yamba/RefreshService.java
+++ Yamba/src/com/twitter/yamba/RefreshService.java
@@ -1,35 +1,67 @@
 package com.twitter.yamba;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-import android.util.Log;
+import java.util.List;
 
-public class RefreshService extends Service {
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClient.Status;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class RefreshService extends IntentService {
 	private static final String TAG = RefreshService.class.getSimpleName();
+
+	public RefreshService() {
+		super(TAG);
+	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		Log.d(TAG, "onCreated");
 	}
-	
+
 	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
+	protected void onHandleIntent(Intent intent) {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		final String username = prefs.getString("username", "");
+		final String password = prefs.getString("password", "");
+
+		// Check that username and password are not empty
+		if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+			Toast.makeText(this, "Please update your username and password",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
 		Log.d(TAG, "onStarted");
-		return super.onStartCommand(intent, flags, startId);
+
+		YambaClient cloud = new YambaClient(username, password);
+		try {
+			List<Status> timeline = cloud.getTimeline(20);
+			for (Status status : timeline) {
+				Log.d(TAG,
+						String.format("%s: %s", status.getUser(),
+								status.getMessage()));
+			}
+		} catch (YambaClientException e) {
+			Log.e(TAG, "Failed to fetch the timeline", e);
+			e.printStackTrace();
+		}
+
+		return;
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		Log.d(TAG, "onDestroyed");
-	}
-	
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
