e3876380800d250eb032424fc3132a5e75ed27e1
Marko Gargenta <marko@marakana.com>
2013-06-13 17:58:37 -0700

    Implemented Location

--- Yamba/src/com/twitter/yamba/YambaWidget.java
+++ Yamba/src/com/twitter/yamba/YambaWidget.java
@@ -3,6 +3,7 @@ package com.twitter.yamba;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,12 +13,22 @@ import android.widget.RemoteViews;
 
 public class YambaWidget extends AppWidgetProvider {
 	private static final String TAG = YambaWidget.class.getSimpleName();
-	
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+		this.onUpdate(context, appWidgetManager, appWidgetManager
+				.getAppWidgetIds(new ComponentName(context, YambaWidget.class)));
+
+	}
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		Log.d(TAG, "onUpdate");
-		
+
 		// Get the latest tweet
 		Cursor cursor = context.getContentResolver().query(
 				StatusContract.CONTENT_URI, null, null, null,
@@ -42,7 +53,7 @@ public class YambaWidget extends AppWidgetProvider {
 			// Update the view
 			RemoteViews view = new RemoteViews(context.getPackageName(),
 					R.layout.list_item);
-			
+
 			// Update the remote view
 			view.setTextViewText(R.id.list_item_text_user, user);
 			view.setTextViewText(R.id.list_item_text_message, message);
